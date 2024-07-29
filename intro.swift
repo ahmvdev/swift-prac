1dfb8050d06037f579ad2dbaefb2edfe33c9f896
ahmvdev <abdullahbasir324@gmail.com>
2024-07-29 18:49:58 +0500

    first commit

---
+++ intro.swift
@@ -0,0 +1,11 @@
+ func introPerson(){   
+        print("Type your Name:")
+        let name = readLine()
+        print("Type your age:")
+        let age = readLine()
+        print("Type your location:")
+        let location = readLine()
+    print("\(name!) is \(age!) years old. And Lives in \(location!)  ")    
+    }
+
+introPerson()
